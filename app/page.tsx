cf3b9aac7ed7e15b5fb838a353cfd0d99dfd50a8
Alimnfl <mnaufallim234@gmail.com>
2025-12-26 16:54:48 +0700

    Modify our features with vector.

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,22 @@
 
 import { useEffect, useRef, useState } from "react";
 import { HandLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
-import { coordinateTrack, Gesture } from "@/lib/coordinate";
 import { sounds } from "@/lib/playSound";
-import { Landmark } from "@/lib/features";
+import { extractVector, Landmark, mirrorVector } from "@/lib/features";
+import { classify, Gesture } from "@/lib/gestures";
+import { drawHand } from "@/lib/drawHand";
+import { SIGN_HUMOR_VECTOR } from "@/fragments/finger/SignHumorData";
 
 export default function HandTracking() {
-  const [coords, setCoords] = useState<Landmark | null>({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
+  const [vectorValue, setVectorValue] = useState<number[] | null>([
+    0, 0, 0, 0, 0,
+  ]);
 
   const [lastGesture, setLastGesture] = useState<Gesture | null>(null);
+  const [handDetected, setHandDetected] = useState(false);
+
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     let landmarker: HandLandmarker | null = null;
@@ -49,6 +52,9 @@ export default function HandTracking() {
       const detect = () => {
         if (!running || !landmarker || !videoRef.current) return;
 
+        const canvasCtx = canvasRef.current?.getContext("2d");
+        if (!canvasCtx) return;
+
         const now = performance.now();
 
         if (now - lastDetect < DETECT_INTERVAL) {
@@ -61,25 +67,35 @@ export default function HandTracking() {
         const results = landmarker.detectForVideo(videoRef.current, now);
 
         if (results.landmarks.length > 0) {
-          const p = results.landmarks[0][8];
-          const gesture = coordinateTrack(results.landmarks[0]);
+          setHandDetected(true);
+          for (let i = 0; i < results.landmarks.length; i++) {
+            const hand = results.landmarks[i];
+            drawHand(canvasCtx, hand);
 
-          if (gesture && gesture !== lastGesture) {
-            sounds[gesture]?.();
-            setLastGesture(gesture);
+            const vector = extractVector(hand);
+            setVectorValue(vector);
+
+            const handLabel =
+              results.handedness && results.handedness[i]
+                ? results.handedness[i]?.[0].categoryName
+                : "right";
+            const processedVector =
+              handLabel === "Left" ? mirrorVector(vector) : vector;
+
+            const gesture = classify(processedVector);
+
+            if (gesture && gesture !== lastGesture) {
+              sounds[gesture]?.();
+              setLastGesture(gesture);
+            }
+
+            if (!gesture) {
+              setLastGesture(null);
+            }
           }
-
-          if (!gesture) {
-            setLastGesture(null);
-          }
-
-          setCoords({
-            x: Number(p.x.toFixed(3)),
-            y: Number(p.y.toFixed(3)),
-            z: Number(p.z.toFixed(3)),
-          });
+        } else {
+          setHandDetected(false);
         }
-
         requestAnimationFrame(detect);
       };
 
@@ -96,32 +112,38 @@ export default function HandTracking() {
 
   return (
     <div className="flex flex-col gap-2 w-full h-full justify-center items-center mx-auto py-20">
-      <h1>Hand Tracking</h1>
-      <video
-        ref={videoRef}
-        autoPlay
-        playsInline
-        muted
-        width={640}
-        height={480}
-        style={{ background: "#000" }}
-      />
+      <h1 className="text-3xl font-semibold">Hand Tracking</h1>
+      <div className="relative">
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          muted
+          width={640}
+          height={480}
+          style={{ background: "#000" }}
+        />
 
-      <div className="flex flex-col gap-2 w-full justify-center items-center">
-        <h3>Coodinate</h3>
-        <div className="flex flex-row w-full justify-between max-w-150">
-          <div className="flex flex-col gap-1">
-            <span>X</span>
-            <span>{coords?.x}</span>
-          </div>
-          <div className="flex flex-col gap-1">
-            <span>Y</span>
-            <span>{coords?.y}</span>
-          </div>
-          <div className="flex flex-col gap-1">
-            <span>Y</span>
-            <span>{coords?.z}</span>
-          </div>
+        <canvas
+          ref={canvasRef}
+          width={640}
+          height={480}
+          className={`top-0 left-0 ${handDetected ? "absolute" : "hidden"}`}
+        />
+      </div>
+
+      <div className="flex flex-col gap-4 w-full justify-center items-center">
+        <h3 className="font-medium">Vector Value</h3>
+        <div className="flex flex-row text-sm w-full justify-between max-w-150">
+          {SIGN_HUMOR_VECTOR.map((item) => (
+            <div
+              key={item.key}
+              className="flex items-center justify-center flex-col gap-1"
+            >
+              <span>{item.title}</span>
+              <span>{vectorValue?.[item.key].toFixed(2) ?? 0}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
