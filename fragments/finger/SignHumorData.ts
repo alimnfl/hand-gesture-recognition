fb8f889b352d4aeb16f80e5f82d444c43c20278d
Alimnfl <mnaufallim234@gmail.com>
2025-12-26 16:55:45 +0700

    Add sign humor.

---
+++ fragments/finger/SignHumorData.ts
@@ -0,0 +1,9 @@
+import { SignHumorVector } from "../type/SignHumorData";
+
+export const SIGN_HUMOR_VECTOR: SignHumorVector[] = [
+  { title: "Thumb → Index", key: 0 },
+  { title: "Index → Middle", key: 1 },
+  { title: "Middle → Ring", key: 2 },
+  { title: "Ring → Pinky", key: 3 },
+  { title: "Wrist → Index", key: 4 },
+];
